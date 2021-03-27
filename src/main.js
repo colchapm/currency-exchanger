0c59cdbbf06a014206b10745486b779e90a2c6c4
Collin Chapman <cchap14@gmail.com>
2021-03-27 16:38:30 -0700

    create functioning input form and user interface logic using hardcoded variables

--- src/main.js
+++ src/main.js
@@ -4,14 +4,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchange from './exchanger.js';
 
+
+function getRates(response) {
+  console.log('inside getrates')
+  console.log(response.result)
+  if (response.result === "success") {
+    $(".showConversion").text(`1 USD = ${response.conversion_rates.JPY} yen`)
+  } else {
+    $(".showErrors").text(`There was an error processing your request ${response.message}`)
+  }
+}
+
+async function makeApiCall(currency) {
+  console.log('inside makeapi call')
+  const response = await CurrencyExchange.getConversion();
+  getRates(response);
+}
+
+
 $(document).ready(function() {
-  $("#getRates").click(function() {
-    let promise = CurrencyExchange.getConversion(currency);
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-        $("#output").text(`${USD} US Dollar = ${currency}`);
-      }, function(error) {
-        $(".showErrors").text(`There was an error processing your request ${error}`);
-    });
+  $("#conversion").click(function() {
+    let currency = $("#convertedCurrency").val();
+    makeApiCall(currency);
   });
 });
+
+// $(document).ready(function() {
+//   $("form#selections").submit(function() {
+//     let promise = CurrencyExchange.getConversion(currency);
+//     promise.then(function(response) {
+//       const body = JSON.parse(response);
+//         $("#output").text(`${USD} US Dollar = ${body.conversion_rates.${convertedCurrency}`);
+//       }, function(error) {
+//         $(".showErrors").text(`There was an error processing your request ${error}`);
+//     });
+//   });
+// });
