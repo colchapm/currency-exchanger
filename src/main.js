6e5289c67ce4f09b1548d2dcc7c08dadf15eb636
Collin Chapman <cchap14@gmail.com>
2021-03-27 15:33:18 -0700

    create user interface scripts file

--- src/main.js
+++ src/main.js
@@ -2,3 +2,16 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchange from './exchanger.js';
+
+$(document).ready(function() {
+  $("#getRates").click(function() {
+    let promise = CurrencyExchange.getConversion(currency);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+        $(".showRates").text(`${USD} US Dollar = ${currency}`);
+      }, function(error) {
+        $(".showErrors").text(`There was an error processing your request ${error}`);
+    });
+  });
+});
