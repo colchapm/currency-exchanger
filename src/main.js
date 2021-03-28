e9f102024c36808f4ceff9c940db832c4f8e8290
Collin Chapman <cchap14@gmail.com>
2021-03-27 20:03:47 -0700

    add currencyText variable

--- src/main.js
+++ src/main.js
@@ -5,20 +5,26 @@ import './css/styles.css';
 import CurrencyExchange from './exchanger.js';
 
 
-function getRates(response, conversionAmount, currencyCode) {
-  if ((response["conversion_rates"][currencyCode]) === undefined) {
+function getRates(response, conversionAmount, currencyCode, currencyText) {
+  if (response["conversion_rates"][currencyCode] === undefined) {
     $(".showErrors").text(`The selected currency code is not available`);
   } else if (response.result === "success") {
-    $(".showConversion").text(response["conversion_rates"][currencyCode]*[conversionAmount]);
+    $(".showConversion").text(`${conversionAmount} USD = ${(response["conversion_rates"][currencyCode]*[conversionAmount]).toLocaleString()} ${currencyText}`);
   } else {
     $(".showErrors").text(`There was an error processing your request: ${response["error-type"]}`);
   }
 }
 
-async function makeApiCall(conversionAmount, currencyCode) {
-  const response = await CurrencyExchange.getConversion();
-  getRates(response, conversionAmount, currencyCode);
+function clearFields() {
+  $(".showConversion").text("");
+  $(".showErrors").text("");
 }
+
+async function makeApiCall(conversionAmount, currencyCode, currencyText) {
+  const response = await CurrencyExchange.getConversion();
+  getRates(response, conversionAmount, currencyCode, currencyText);
+}
+
 
 
 $(document).ready(function() {
@@ -26,6 +32,9 @@ $(document).ready(function() {
     let conversionAmount = $("#usd-amount").val();
     const inputCurrency = $("#convertedCurrency").val();
     let currencyCode = inputCurrency.substring(0,3);
-    makeApiCall(conversionAmount, currencyCode);
+    let currencyText = inputCurrency.substring(6);
+    clearFields();
+    makeApiCall(conversionAmount, currencyCode, currencyText);
+    $("#output").show();
   });
 });
