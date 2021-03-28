1dfbbcece5b03d74b6aaa3fa14cc8c7cb6866085
Collin Chapman <cchap14@gmail.com>
2021-03-27 18:40:00 -0700

    add currency descriptions to makem more user friendly

--- src/exchanger.js
+++ src/exchanger.js
@@ -1,6 +1,5 @@
 export default class CurrencyExchange {
   static async getConversion() {
-    console.log('inside class')
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       if (!response.ok) {
