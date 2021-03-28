889d3da11236ca0317db859d2e17a204df9e5289
Collin Chapman <cchap14@gmail.com>
2021-03-27 18:58:29 -0700

    parse json correctly to show error type

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ function getRates(response, conversionAmount, currencyCode) {
   if (response.result === "success") {
     $(".showConversion").text(response["conversion_rates"][currencyCode]*[conversionAmount]);
   } else {
-    $(".showErrors").text(`There was an error processing your request ${response.message}`);
+    $(".showErrors").text(`There was an error processing your request: ${response["error-type"]}`);
   }
 }
 
